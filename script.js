ffe20c34db8fcf29b3259a8d93bb89270c93cbe5
asnaaslam1 <asnaaslam91@gmail.com>
2026-06-12 21:47:07 +0530

    updated

--- script.js
+++ script.js
@@ -1,60 +1,38 @@
-function showRegister() {
-document.getElementById("login").classList.add("hide");
-document.getElementById("register").classList.remove("hide");
+function showRegister(){
+document.getElementById("loginBox").classList.add("hide");
+document.getElementById("registerBox").classList.remove("hide");
 }
 
-function showLogin() {
-document.getElementById("register").classList.add("hide");
-document.getElementById("login").classList.remove("hide");
+function showLogin(){
+document.getElementById("registerBox").classList.add("hide");
+document.getElementById("loginBox").classList.remove("hide");
 }
 
-const loginBtn = document.querySelector("#login .btn");
-
-if(loginBtn){
-loginBtn.addEventListener("click",function(){
-
-const email =
-document.querySelector("#login input[type='email']").value;
-
-const password =
-document.querySelector("#login input[type='password']").value;
+function login(){
+let email = document.querySelector("#loginBox input[type='email']").value;
+let password = document.querySelector("#loginBox input[type='password']").value;
 
 if(email==="" || password===""){
-alert("Please fill all fields");
-}
-else{
+alert("Please fill all login fields");
+}else{
 alert("Login Successful");
 }
-});
 }
 
-const registerBtn =
-document.querySelector("#register .btn");
-
-if(registerBtn){
-registerBtn.addEventListener("click",function(){
-
-const username =
-document.querySelector("#register input[type='text']").value;
-
-const email =
-document.querySelector("#register input[type='email']").value;
-
-const password =
-document.querySelector("#register input[type='password']").value;
-
-const terms =
-document.querySelector("#register .terms input").checked;
+function register(){
+let username = document.querySelector("#registerBox input[type='text']").value;
+let email = document.querySelector("#registerBox input[type='email']").value;
+let password = document.querySelector("#registerBox input[type='password']").value;
+let terms = document.querySelector("#registerBox input[type='checkbox']").checked;
 
 if(username==="" || email==="" || password===""){
 alert("Please fill all fields");
 }
 else if(!terms){
-alert("Accept Terms & Conditions");
+alert("Please accept Terms & Conditions");
 }
 else{
 alert("Registration Successful");
 showLogin();
 }
-});
 }
