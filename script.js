fb73ae8bc3950f98927ce8cbda70b7798b992fab
asnaaslam1 <asnaaslam91@gmail.com>
2026-06-12 22:56:21 +0530

    update

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
+function hideAll(){
+document.querySelectorAll(".card").forEach(card=>{
+card.classList.remove("active");
+});
+}
+
 function showLogin(){
 hideAll();
-document.getElementById("loginSection").classList.add("active");
+document.getElementById("loginBox").classList.add("active");
 }
 
 function showRegister(){
 hideAll();
-document.getElementById("registerSection").classList.add("active");
+document.getElementById("registerBox").classList.add("active");
 }
 
 function showForgot(){
 hideAll();
-document.getElementById("forgotSection").classList.add("active");
-}
-
-function showHome(){
-hideAll();
-document.getElementById("homeSection").classList.add("active");
-}
-
-function hideAll(){
-document.querySelectorAll(".section").forEach(sec=>{
-sec.classList.remove("active");
-});
+document.getElementById("forgotBox").classList.add("active");
 }
